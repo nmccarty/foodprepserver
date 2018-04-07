87c6cbc9b5921616e42a3e9e1249eaadfb1edcd2
Nathan McCarty <nathan@mccarty.io>
2018-04-07 17:35:53 -0400

    Lots of changes

--- src/food/mod.rs
+++ src/food/mod.rs
@@ -1,6 +1,8 @@
+#[derive(PartialEq,Clone)]
 pub enum Unit {
     Liters(f64),
     Grams(f64),
+    Nothing
 }
 
 impl Unit {
@@ -20,15 +22,42 @@ impl Unit {
         match *self {
             Unit::Liters(_) => Unit::Liters(ammount),
             Unit::Grams(_) => Unit::Grams(ammount),
+            Unit::Nothing => Unit::Nothing,
         }
     }
 }
 
+#[derive(PartialEq,Clone)]
 pub struct Ingredient {
     name: String,
     proto_unit: Unit,
 }
 
+impl Ingredient {
+    pub fn new(name: String, proto: Unit) -> Ingredient{
+        Ingredient {
+            name: name,
+            proto_unit: proto.zero(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum FoodType {
+    Ingredient(Ingredient),
+    Reicpe(Recipe),
+}
+
+impl FoodType {
+    pub fn new_ingredient(ingredient: Ingredient) -> FoodType {
+        FoodType::Ingredient(ingredient)
+    }
+    pub fn new_reicpe(recipe: Recipe) -> FoodType {
+        FoodType::Reicpe(recipe)
+    }
+}
+
+#[derive(Clone)]
 pub struct Recipe {
     components: Vec<Food>,
     steps: Vec<String>,
@@ -38,11 +67,19 @@ pub struct Recipe {
     takes: i32,
 }
 
-pub enum FoodType {
-    Ingredient(Ingredient),
-    Reicpe(Recipe),
+impl Recipe {
+    pub fn new_empty() -> Recipe {
+        Recipe {
+            components: Vec::new(),
+            steps: Vec::new(),
+            produces: Unit::Nothing,
+            name: String::new(),
+            takes: 0,
+        }
+    }
 }
 
+#[derive(Clone)]
 pub struct Food {
     food: FoodType,
     ammount: Unit,
