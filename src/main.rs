e80c475bd1e0028e4319cbeccd57d0d4e779b984
Nathan McCarty <nathan@mccarty.io>
2018-04-08 10:49:06 -0400

    Add json parsing input

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,39 @@ use rustful::context::body::BodyReader;
 use std::io::Read;
 use hyper::header::{AccessControlAllowOrigin, Headers};
 
+use serde_json::{Value, Error};
+
 use food::*;
 use food::food_lib::FOOD_LIB;
 use food::engine::*;
 
-struct FoodPlan(Vec<Vec<Dish>>);
+struct FoodPlan;
 
 impl Handler for FoodPlan {
     fn handle_request(&self, mut context: Context, response: Response) {
         let mut string = String::new();
         context.body.read_to_string(&mut string);
 
-        println!("{}", string);
+        let mut day_times = Vec::new();
+        let mut day_avails = Vec::new();
+
+        for i in (0..7) {
+            day_times.push(9);
+            day_avails.push(30);
+        }
+
+        let json_value: Value = serde_json::from_str(&string).ok().unwrap();
+        let values = json_value.as_array().unwrap();
+
+        for value in values {
+            let day = value["day"].as_u64().unwrap() as usize;
+            let time = value["time"].as_i64().unwrap() as i32;
+            let start = value["start"].as_i64().unwrap() as i32;
+
+            day_times[day] = start;
+            day_avails[day] = time;
+            println!("day: {} start: {} time: {}", day, start, time);
+        }
 
         let mut engine = SuggestionEngine::new();
 
@@ -46,54 +67,12 @@ fn addACA(response: &mut Response) {
 }
 
 fn main() {
-    let nudes = Ingredient::new("noodles", GRAMS).to_food(NOTHING, true);
-    let sauce = Ingredient::new("sauce", LITERS).to_food(NOTHING, true);
-    let alfredo = Ingredient::new("alfredo", LITERS).to_food(NOTHING, true);
-    let chips = Ingredient::new("chips", GRAMS).to_food(NOTHING, true);
-
-    let spa = Recipe::new("Spaghet")
-        .add_component(&nudes)
-        .add_component(&sauce)
-        .add_step("Cook noodles")
-        .add_step("Apply sauce to noodles")
-        .add_step("Enjoy")
-        .set_takes(30);
-
-    let spa_dish = FOOD_LIB[5].to_dish(9);
-
-    let alfred = Recipe::new("Alfred")
-        .add_component(&nudes)
-        .add_component(&alfredo)
-        .add_step("Cook noodles (al dente)")
-        .add_step("Apply alfredo sauce to noodles")
-        .add_step("Enjoy alfredoly")
-        .set_takes(45);
-
-    let alfred_dish = Food::from_recipe(alfred, Unit::Nothing).to_dish(8);
-
-    let mut meals = Vec::new();
-
-    for x in 0..7 {
-        let mut vec = Vec::new();
-        if x % 2 == 0 {
-            vec.push(spa_dish.clone());
-        } else {
-            vec.push(alfred_dish.clone());
-        }
-
-        if x % 3 == 0 {
-            vec.push(chips.to_dish(17));
-        }
-
-        meals.push(vec);
-    }
-
     let my_router = insert_routes!{
         //Create a new TreeRouter
         TreeRouter::new() => {
             "foodplan" => {
-                Get: FoodPlan(meals.clone()),
-                Post: FoodPlan(meals.clone()),
+                Get: FoodPlan,
+                Post: FoodPlan,
             }
         }
     };
